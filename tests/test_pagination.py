405338d23899f1511cfd0c4360faae3a26beee11
biryukov <biryukov.aa@niistt.ru>
2024-07-18 22:41:22 +0300

    fix pagination

--- tests/test_pagination.py
+++ tests/test_pagination.py
@@ -8,18 +8,37 @@ async def test_concurrent_requests(b24):
     with aioresponses() as m:
         m.get(
             "https://example.bitrix24.com/rest/1/123456789/crm.deal.list.json?start=0",
-            payload={"result": [{"ID": 1}], "next": 50, "total": 100},
+            payload={"result": [{"ID": 1}], "next": 50, "total": 82},
             status=200,
             repeat=True,
         )
         m.get(
             "https://example.bitrix24.com/rest/1/123456789/crm.deal.list.json?start=50",
-            payload={"result": [{"ID": 2}], "total": 100},
+            payload={"result": [{"ID": 2}], "total": 82},
             status=200,
             repeat=True,
         )
         res = await b24.callMethod("crm.deal.list")
         assert res == [{"ID": 1}, {"ID": 2}]
+
+
+@pytest.mark.asyncio
+async def test_concurrent_requests_nesting_level(b24):
+    with aioresponses() as m:
+        m.get(
+            "https://example.bitrix24.com/rest/1/123456789/tasks.task.list.json?start=0",
+            payload={"result": {"tasks": [{"ID": 1}]}, "next": 50, "total": 100},
+            status=200,
+            repeat=True,
+        )
+        m.get(
+            "https://example.bitrix24.com/rest/1/123456789/tasks.task.list.json?start=50",
+            payload={"result": {"tasks": [{"ID": 2}]}, "total": 100},
+            status=200,
+            repeat=True,
+        )
+        res = await b24.callMethod("tasks.task.list")
+        assert res == {"tasks": [{"ID": 1}, {"ID": 2}]}
 
 
 @pytest.mark.asyncio
